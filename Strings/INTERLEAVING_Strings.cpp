eb1f002435ff617fc9ef5f0ee2fa52c777e4d778
PIYUSH NAUTIYAL <nautiyalpiyush54@gmail.com>
2025-07-25 22:03:53 +0530

    Strings Day 4

--- Strings/INTERLEAVING_Strings.cpp
+++ Strings/INTERLEAVING_Strings.cpp
@@ -1,4 +1,4 @@
-/* Link : https://www.geeksforgeeks.org/problems/interleaved-strings/1#naive-approach-using-recursion-o2mn-time-and-omn-space
+/* Link : https://www.geeksforgeeks.org/problems/interleaved-strings/1#expected-approach-spaceoptimized-dp-on-m-time-and-om-space
 Interleaved Strings
 Difficulty: MediumAccuracy: 24.33%Submissions: 101K+Points: 4Average Time: 30m
 Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
@@ -18,8 +18,7 @@ Explanation: s3 has all characters of s1 and s2 and retains order of characters 
 */
 #include<bits/stdc++.h>
 using namespace std;
-
-  class Solution {
+class Solution {
   public:
   bool memo_solve(string& s1, string& s2, string& s3,int m,int n, int N,int i ,int j,vector<vector<int>>&dp){
         
@@ -30,26 +29,68 @@ using namespace std;
             return dp[i][j];
             
         } 
-        bool a  = (i<m && s1[i]==s3[k] && solve(s1,s2,s3,m,n,N,i+1,j,dp));
-        bool b =  (j<n && s2[j]==s3[k] && solve(s1,s2,s3,m,n,N,i,j+1,dp));
+        bool a  = (i<m && s1[i]==s3[k] && memo_solve(s1,s2,s3,m,n,N,i+1,j,dp));
+        bool b =  (j<n && s2[j]==s3[k] && memo_solve(s1,s2,s3,m,n,N,i,j+1,dp));
         return dp[i][j] = a || b; 
     }
+    bool tabu_solve(string& s1, string& s2, string& s3,vector<vector<bool>>& dp){
+        int m= s1.size(),n=s2.size(),N = s3.size();
+        
+        dp[0][0] = true ;
+        // if s1 is empyty then s2 will match with s3 
+        for (int i =1;i<=n;i++){
+            dp[0][i] = dp[0][i-1]&&(s2[i-1]==s3[i-1]);
+        }
+        // if s2 is empyty then s1 will match with s3 
+        for (int i =1;i<=m;i++){
+            dp[i][0] = dp[i-1][0]&&(s1[i-1]==s3[i-1]);
+        }
+        // filling rest of dp table 
+        for (int i =1;i<=m;i++){
+            for (int j = 1;j<=n;j++){
+                dp[i][j] = (dp[i-1][j]&&s1[i-1]==s3[i+j-1])|| (dp[i][j-1]&&s2[j-1]==s3[i+j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+    
+    /// Optimal  space optimized 
+    bool optimized(string& s1, string& s2, string& s3){
+        int m = s1.size(),n=s2.size(),N = s3.size();
+        vector<bool>prev(n+1) ;
+        vector<bool>curr(n+1);
+        prev[0] = true ;
+        // Computing prev using only s2  (when s1 is empty)
+
+        for (int j =1 ;j<=n;j++){
+            prev[j] =( s2[j-1]==s3[j-1])&& prev[j-1];
+            
+        }
+        // Process all characters of s1 and s2
+        for (int i =1 ;i<=m;i++){
+            curr[0] =  (s1[i-1]==s3[i-1])&& prev[i-1];
+            for (int j =1 ;j<=n;j++){
+                curr[j] = (s1[i-1] == s3[i+j-1]&& curr[j-1] ) ||  s2[j-1]==s3[i+j-1]&& prev[j-1];
+                
+            }
+            prev = curr;
+        }
+        return curr[n];
+    }
+
     bool isInterleave(string& s1, string& s2, string& s3) {
         /// Recursive Approach Not Optimal 
         int m= s1.size(),n=s2.size(),N = s3.size();
         if (m+n != N)return false ;
         int i =0 ,j=0,k=0;
-        //  return solve(s1,s2,s3,m,n,N,i,j,k);
-
+        
         // [Better Approach 1] Using Top-Down Memoization DP - O(m*n) Time and O(n*m) Space
     
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return memo_solve(s1,s2,s3,m,n,N,i,j,dp);
-        
+        vector<vector<bool>>dp(m+1,vector<bool>(n+1,-1));
+        // return memo_solve(s1,s2,s3,m,n,N,i,j,dp);
+        return o(s1,s2,s3);
     }
 };
-
-
 int main() {
     int t;
     cin >> t;
