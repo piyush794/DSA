664de3be5ba0bae4da696fe5baa6228734dceb37
PIYUSH NAUTIYAL <nautiyalpiyush54@gmail.com>
2025-11-30 23:32:03 +0530

    Strings Day 17

--- Strings/Rabin-Karp.cpp
+++ Strings/Rabin-Karp.cpp
@@ -26,12 +26,28 @@ Output: 2 7 13
 
 Explanation: The pattern ‘pattern’ = “xyz” appears at 3 positions in ‘text’.
 */
+
 #include<iostream>
 #include <cmath>
 using namespace std ;
 vector<int>ans;
 int n ;
-/* Brute Force Technique without Rolling Hash */
+/* Brute Force through Sliding Window + String Compare 
+*/
+
+vector <int > Brute_stringMatch(string text, string pattern) {
+	int m = pattern.size();
+	int n = text.size();
+	for (int i = 0 ;i<= n-m ; i++ ){
+		string temp = text.substr(i, m);
+		if (temp == pattern){
+			ans.push_back(i+1);
+		}
+	}
+	return ans;
+}
+
+/* Basic Rabin Karp Technique without Rolling Hash */
 long long ipow(long long base, int exp) {
     long long result = 1;
     while (exp--) result *= base;
@@ -46,7 +62,7 @@ long long  lps(string pattern){
 	}
 	return hashcode;
 }
-vector<int> stringMatch(string text, string pattern) {
+vector<int> RabinBrutestringMatch(string text, string pattern) {
 
 	n = text.size();
 	int m = pattern.size();
@@ -64,3 +80,81 @@ vector<int> stringMatch(string text, string pattern) {
 	return ans;
 }
 
+
+/* Most Optimal Rabin Karp Algorithm with Double Rolling HASH correctly implemented*/
+#define ll long long int 
+
+const ll RADIX_1 = 26;
+const ll RADIX_2 = 27;
+const ll MOD_1 = 1000000029;
+const ll MOD_2 = 1000000033;
+
+
+pair<ll ,ll > get_hash_Pair(string pattern , int m ){
+	ll  hash1 = 0 , hash2 = 0 ;
+	ll factor1 =  1  ,factor2 = 1;
+	
+	for (ll i = m-1 ; i>=0 ;i--){
+		hash1 += ((int)pattern[i]*factor1)%MOD_1 ; 
+		factor1 = (factor1 * RADIX_1) %MOD_1 ; 
+		hash2 += ((int)pattern[i] * factor2 )%MOD_2 ;
+		factor2 = (factor2 * RADIX_2 ) % MOD_2 ; 
+
+	}
+	return {hash1 % MOD_1, hash2%MOD_2 } ;
+
+}
+vector<int> stringMatch(string text, string pattern) {
+	
+	int n = text.size() ;
+	int m = pattern.size() ;
+	if (n<m )return {-1};
+	ll MAX_WEIGHT_1 = 1 ;
+	ll MAX_WEIGHT_2 = 1 ;
+	for (int i=1 ;i<m ;i++){
+		MAX_WEIGHT_1 = (MAX_WEIGHT_1 * RADIX_1) % MOD_1 ; 
+		MAX_WEIGHT_2 = (MAX_WEIGHT_2 * RADIX_2) % MOD_2 ;
+	}
+	pair<ll , ll>hashcode = get_hash_Pair(pattern ,m);
+	pair<ll , ll>texthash = {0 , 0 };
+
+	for (int i =0 ;i<=n-m ;i++ ){
+		if (i == 0 ){
+			texthash = get_hash_Pair(text , m);
+		}
+		else{
+			texthash.first =
+						(
+							( (texthash.first
+								- ( (long long)(text[i-1]) * MAX_WEIGHT_1 ) % MOD_1
+								+ MOD_1 ) % MOD_1
+							* RADIX_1)
+							+ (long long)(text[i+m-1])
+						) % MOD_1;
+
+					texthash.second =
+						(
+							( (texthash.second
+								- ( (long long)(text[i-1]) * MAX_WEIGHT_2 ) % MOD_2
+								+ MOD_2 ) % MOD_2
+							* RADIX_2)
+							+ (long long)(text[i+m-1])
+						) % MOD_2;
+		}
+		if (texthash.first == hashcode.first && texthash.second == hashcode.second){
+			ans.push_back(i+1);
+		}
+	}
+	return ans;
+
+}
+int main(){
+	string pattern;
+	string text;
+	cin>> text ;
+	cin >> pattern ; 
+	vector<int> result  = stringMatch(text , pattern);
+	for (auto I : result){
+		cout << I << " ";
+	}
+}
